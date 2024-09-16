ab0b5fa0dff78afba3427d2b03eb829c4314ff6e
Timo <537181@student.fontys.nl>
2024-09-16 11:00:58 +0200

    Main func toegevoegd

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// functie om de juiste bestanden te kunnen verplaatsen naar de juiste map
+func moveFile(srcPath, destDir string)
+
+func sortFiles(srcDir) error {
+	entries, err := os.ReadDir(srcDir)
+	if err != nill {
+		return fmt.Errorf("Fout bij het lezen van map: %v", err)
+	}
+
+}
+
+func main() {
+	// Hier geef je aan in welke map het gesorteerd wordt
+	srcDir := "C:\\Users\\TimovG.DESKTOP-238SGRN\\Documents\\Fontys\\Semester 2-2\\Applicatie\\TEST"
+
+	// Sorteer de bestanden in de juiste opgegeven map
+	err := sortFiles(srcDir)
+	if err != nill {
+		fmt.Println("Er is een fout opgetreden: %v\n", err)
+	} else {
+		fmt.Println("Bestand succesvol gesorteerd")
+	}
+}
